402a06c186081693757be10bddd61822799c3da1
iyang <931032337@qq.com>
2019-02-15 20:48:13 +0800

    条件查询显示

--- mall_search_web/src/main/webapp/js/controller/searchController.js
+++ mall_search_web/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,15 @@
 app.controller('searchController',function($scope,searchService){
+
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{}};//搜索对象
+    //添加搜索项
+    $scope.addSearchItem=function(key,value){
+        if(key=='category' || key=='brand'){//如果点击的是分类或者是品牌
+            $scope.searchMap[key]=value;
+        }else{
+            $scope.searchMap.spec[key]=value;
+        }
+    }
+
     //搜索
     $scope.search=function(){
         searchService.search( $scope.searchMap ).success(
