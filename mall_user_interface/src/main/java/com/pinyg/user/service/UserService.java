aec4dd84f2613cfd2e030f996a96947e0693f838
iyang <931032337@qq.com>
2019-02-20 19:24:37 +0800

    用户控制中心环境搭建

---
+++ mall_user_interface/src/main/java/com/pinyg/user/service/UserService.java
@@ -0,0 +1,63 @@
+package com.pinyg.user.service;
+
+import com.pinyg.pojo.TbUser;
+import entity.PageResult;
+
+import java.util.List;
+
+/**
+ * 服务层接口
+ * @author Administrator
+ *
+ */
+public interface UserService {
+
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	public List<TbUser> findAll();
+	
+	
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+	
+	
+	/**
+	 * 增加
+	*/
+	public void add(TbUser user);
+	
+	
+	/**
+	 * 修改
+	 */
+	public void update(TbUser user);
+	
+
+	/**
+	 * 根据ID获取实体
+	 * @param username
+	 * @return
+	 */
+	public TbUser findOne(String username);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+
+	/**
+	 * 分页
+	 * @param pageNum 当前页 码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbUser user, int pageNum, int pageSize);
+
+}
---
+++ mall_user_web/src/main/java/com/pinyg/user/controller/UserController.java
@@ -0,0 +1,130 @@
+package com.pinyg.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyg.pojo.TbUser;
+import com.pinyg.user.service.UserService;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * controller
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Reference
+	private UserService userService;
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findAll")
+	public List<TbUser> findAll(){			
+		return userService.findAll();
+	}
+	
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult  findPage(int page,int rows){			
+		return userService.findPage(page, rows);
+	}
+	
+	/**
+	 * 增加
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbUser user){
+		try {
+			userService.add(user);
+			return new Result(true, "增加成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "增加失败");
+		}
+	}
+	
+	/**
+	 * 修改
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbUser user){
+		try {
+			userService.update(user);
+			return new Result(true, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "修改失败");
+		}
+	}	
+	
+	/**
+	 * 获取实体
+	 * @param username
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbUser findOne(String username){
+		return userService.findOne(username);
+	}
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long [] ids){
+		try {
+			userService.delete(ids);
+			return new Result(true, "删除成功"); 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "删除失败");
+		}
+	}
+	
+		/**
+	 * 查询+分页
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(@RequestBody TbUser user, int page, int rows  ){
+		return userService.findPage(user, page, rows);		
+	}
+
+	/**
+	 * 用户登录
+	 * @return
+	 */
+	@RequestMapping("/getUserName")
+	public Map name(){
+		String name= SecurityContextHolder.getContext()
+				.getAuthentication().getName();
+		Map map=new HashMap();
+		map.put("userName", name);
+		return map ;
+	}
+
+}
